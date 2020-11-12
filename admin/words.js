cdcb1512cd1e31033c43e5b5fe51987041a52292
root <root@ioBroker-Master.yemut.local>
2020-11-12 08:47:15 +0000

    initial version

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"network-monitoring adapter settings": {
+		"en": "Adapter settings for network-monitoring",
+		"de": "Adaptereinstellungen für network-monitoring",
+		"ru": "Настройки адаптера для network-monitoring",
+		"pt": "Configurações do adaptador para network-monitoring",
+		"nl": "Adapterinstellingen voor network-monitoring",
+		"fr": "Paramètres d'adaptateur pour network-monitoring",
+		"it": "Impostazioni dell'adattatore per network-monitoring",
+		"es": "Ajustes del adaptador para network-monitoring",
+		"pl": "Ustawienia adaptera dla network-monitoring",
+		"zh-cn": "network-monitoring的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
